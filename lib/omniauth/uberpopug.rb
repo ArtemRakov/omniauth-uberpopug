9272a68de2fe6192e1e48a56e9d21792be13da85
Artem Rakov <artem@rakov.me>
2021-11-11 10:21:09 +0000

    init

---
+++ lib/omniauth/uberpopug.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "uberpopug/version"
+
+module Omniauth
+  module Uberpopug
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/uberpopug/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Omniauth
+  module Uberpopug
+    VERSION = "0.1.0"
+  end
+end
